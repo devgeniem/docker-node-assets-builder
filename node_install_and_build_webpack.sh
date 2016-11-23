84bb4e5df4751494139909469339957a3742ea6c
Onni Hakala <onni@keksi.io>
2016-11-23 18:39:33 +0200

    Enhance install script

--- node_install_and_build_webpack.sh
+++ node_install_and_build_webpack.sh
@@ -1,13 +1,35 @@
 #!/bin/bash
+##
+# This script installs npm and builds webpack in all directories given as argument
+##
+if [[ $# -eq 0 ]] ; then
+    echo "Usage: $0 /directory/to/install/npm/"
+    exit 0
+fi
 
-# Rename variables
-dir=$1
+for dir in "$@"
+do
 
-# Go to directory
-cd $dir
+    # Go to directory
+    echo "Checking directory $dir"
+    cd $dir
 
-# Install npm
-npm install
+    # Install npm
+    if [ -f package.json ];
+    then
+        echo "Running npm install..."
+        npm install
+    else
+       echo "No package.json present, skipping..."
+    fi
 
-# Run included webpack
-./node_modules/webpack/bin/webpack.js
+    # Install npm
+    if [ -f ./node_modules/webpack/bin/webpack.js ];
+    then
+        # Run using included webpack
+        echo "Running ./node_modules/webpack/bin/webpack.js..."
+        ./node_modules/webpack/bin/webpack.js
+    else
+        echo "No local webpack.js present, skipping..."
+    fi
+done
